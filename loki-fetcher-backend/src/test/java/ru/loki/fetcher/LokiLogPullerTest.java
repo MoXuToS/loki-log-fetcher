ce6451212deb0ce6084131e1511b9d3b1dfbd4a2
Долгов Данила Александрович <ddolgov@phoenixit.ru>
2026-04-16 17:23:13 +0300

    Добавлен тест на поднятие контекста приложения

---
+++ loki-fetcher-backend/src/test/java/ru/loki/fetcher/LokiLogPullerTest.java
@@ -0,0 +1,18 @@
+package ru.loki.fetcher;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Тест на поднятие контекста.
+ */
+@SpringBootTest
+public class LokiLogPullerTest {
+
+    @Test
+    @DisplayName("Тест на поднятие контекста")
+    void contextLoad() {
+
+    }
+}
